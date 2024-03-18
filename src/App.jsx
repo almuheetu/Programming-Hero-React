205a0667ad3fada9385d2257be4c444b503ce27b
Almuheetu Shihab <almuheetu@gmail.com>
2024-03-18 21:54:39 +0600

    Advance rendering  practice

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './Todo'
+import Actor from './Actor'
+import Singer from './singer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const actors = ['Saib', 'jasim', 'rahim', 'korim', 'jasimsss', 'salmanshah'];
+
+  const singers = [
+    {id: 1, name: 'Dr. Mahfuzur Rahman', age: 68},
+    {id: 2, name: 'Zunayed Evan', age: 38},
+    {id: 3, name: 'Salma', age: 35},
+    {id: 4, name: 'Pritom', age: 30}
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+ 
       <h1>Vite + React</h1>
-      <Todo task="Learn React" isDone={true}></Todo>
+
+      {
+        singers.map(singer => <Singer singer={singer}></Singer>)
+      }
+
+
+      <Actor name={"Almuheetu Shihab"}></Actor>
+      {
+        actors.map(actor => <Actor name={actor}></Actor>)
+      }
+      {/* <Todo task="Learn React" isDone={true}></Todo>
       <Todo task="Try React"></Todo>
       <Todo task="custom React" isDone={false}></Todo>
-      <Todo task="jsx React" isDone={true}></Todo>
+      <Todo task="jsx React" isDone={true}></Todo> */}
      
     </>
   )
